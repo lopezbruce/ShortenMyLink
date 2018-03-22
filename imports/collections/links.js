ed93a21fa151a1558882bef21c192dcd5e93fe7e
Bruce Lopez <blopez6@uncc.edu>
2018-03-22 08:44:28 -0400

    added logic to validate the url

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,8 +1,10 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 Meteor.methods({
-  'links.insert': function (url){
-    console.log('attempting to save', url)
+  'links.insert': function(url) {
+    check(url, Match.Where(url => validUrl.isUri(url)));
   }
 });
 
